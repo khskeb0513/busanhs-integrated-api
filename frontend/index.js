ee6a9f2f158740d05e5899a5d4274a72c28d432f
khskeb0513 <khskeb0513@gmail.com>
2020-11-28 01:02:12 +0900

    index title edited

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('index', {
-        title: 'BusanHighSchool e-haksa'
+        title: 'Busan HighSchool e-haksa'
     })
 })
 
